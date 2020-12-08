f3f2cbd9bbc6392cbc9b33ad79b07bf34adcc7d8
Joseph Heyburn <34041368+jdheyburn@users.noreply.github.com>
2020-12-08 17:25:42 +0000

    fix(git): silently fail in `git_main_branch` if not in a git repo (#9484)
    Co-authored-by: Marc Cornellà <marc.cornella@live.com>

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -34,11 +34,11 @@ function git_develop_branch() {
 # Check if main exists and use instead of master
 function git_main_branch() {
   command git rev-parse --git-dir &>/dev/null || return
-  local ref
-  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,stable,master}; do
-    if command git show-ref -q --verify $ref; then
-      echo ${ref:t}
-      return 0
+  local branch
+  for branch in main trunk; do
+    if command git show-ref -q --verify refs/heads/$branch; then
+      echo $branch
+      return
     fi
   done
 
